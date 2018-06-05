5728b7f98d2a6f6fc9d8896fbbf2713c77ab5de1
zhoulin <zhoulin_ch@126.com>
2018-06-05 10:31:54 +0800

    template form 方式校验表单

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User} from '../basic-data/user';
+import { User } from '../basic-data/user';
 // 添加校验
 import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
@@ -11,10 +11,13 @@ import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, Valid
 })
 export class LoginComponent implements OnInit {
   user: User = new User();
-  heroForm: FormGroup;
+
+  // 验证用FormGroup 自定义校验，减少代码量
+  userGroup: FormGroup;
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
